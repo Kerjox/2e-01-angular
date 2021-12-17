5da93fe22d18c682896404a0fa6dee3f0845d614
kerjox <kerjoxgamer095@gmail.com>
2021-12-17 14:00:40 +0100

    Agregado alumno component

---
+++ src/app/alumno/alumno.component.ts
@@ -0,0 +1,26 @@
+import {Component, OnInit} from '@angular/core';
+
+
+@Component({
+  selector: 'app-alumno',
+  templateUrl: './alumno.component.html',
+  styleUrls: ['./alumno.component.css']
+})
+export class AlumnoComponent implements OnInit {
+
+  name: string = 'Alita';
+  surname: string = 'Battle Angel';
+  direction: string = 'Iron City';
+  age: number = 224;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  getNombreCompleto(): string {
+
+    return ''.concat(this.name, ' ', this.surname);
+  }
+
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
-  title = 'AngularPrueba';
+  title = 'Angular Ubalde';
+  header = null;
+  imagen: string = 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fimages5.alphacoders.com%2F531%2F531494.jpg&f=1&nofb=1';
+
+  name: string = 'Alita'
+  surname: string = 'Battle Angel'
+
+  getCompleteName(): string {
+
+    return ''.concat(this.name, ' ', this.surname);
+  }
 }
