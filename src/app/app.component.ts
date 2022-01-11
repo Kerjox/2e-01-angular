22564958e2cfa70915cd63bbdbab07b50b7ab3ff
kerjox <kerjoxgamer095@gmail.com>
2022-01-11 09:41:49 +0100

    Added button with state

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,18 +3,21 @@ import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html'
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'Angular Ubalde';
   header = null;
   imagen: string = 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fimages5.alphacoders.com%2F531%2F531494.jpg&f=1&nofb=1';
 
+  buttonState:boolean = true;
+
   name: string = 'Alita'
   surname: string = 'Battle Angel'
 
   getCompleteName(): string {
 
-    return ''.concat(this.name, ' ', this.surname);
+    return this.name + ' ' + this.surname;
   }
 }
